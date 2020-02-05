641e470e7fe8e5617da9c1be8114caac0b588d74
RayWu <rytesfabc@gmail.com>
2020-02-05 14:28:34 +0800

    add python requires

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ setup(name='print-function',
       url='https://github.com/raywu60kg/print-function',
       author='raywu60kg',
       author_email='wuhaohsiang1992@gmail.com',
-      packages=['___future___'])
+      packages=['___future___'],
+      license="MIT",
+      python_requires="3.4*,3.5*,3.6*,3.7*,3.8*")
