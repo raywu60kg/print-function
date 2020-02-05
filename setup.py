b863673d3b8b9e8c597b62a1d8521cad94d4150c
RayWu <rytesfabc@gmail.com>
2020-02-05 14:43:23 +0800

    udpate setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
+import os 
+import codecs
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
 
 setup(name='print-function',
-      version='1.0.0',
+      version='1.0.1',
       description='Best python print function',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/raywu60kg/print-function',
       author='raywu60kg',
       author_email='wuhaohsiang1992@gmail.com',
       packages=['___future___'],
       license="MIT",
-      python_requires="==3.4*,==3.5*,==3.6*,==3.7*,==3.8*")
+      python_requires=">=3.4")
